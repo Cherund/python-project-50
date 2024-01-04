225b63efabc26e110d4332b857d755d1c19c8f8e
Egor Gerimovich <cherund@gmail.com>
2024-01-04 13:47:50 +0100

    edited cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -2,4 +2,4 @@ def get_paths(args):
     if isinstance(args, list):
         return {'first_file': args[0], 'second_file': args[1]}
 
-    return {'first_file':args.first_file, 'second_file': args.second_file}
+    return {'first_file': args.first_file, 'second_file': args.second_file}
