ede5b7369188ef5d696d799c4ffb68fd4dae7ad4
Egor Gerimovich <cherund@gmail.com>
2024-01-07 15:53:10 +0100

    add nested yaml check

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -8,6 +8,7 @@ def test_flat_json_diff():
     file1_path = f'{FIXTURES_DIR}/file1.json'
     file2_path = f'{FIXTURES_DIR}/file2.json'
     with open(f'{FIXTURES_DIR}/result') as output:
+        print(generate_diff(file1_path, file2_path))
         assert generate_diff(file1_path, file2_path) == output.read()
 
 
@@ -16,4 +17,18 @@ def test_flat_yml_diff():
     file2_path = f'{FIXTURES_DIR}/file2.yml'
 
     with open(f'{FIXTURES_DIR}/result') as output:
+        assert generate_diff(file1_path, file2_path) == output.read()
+
+
+def test_nested_json_diff():
+    file1_path = f'{FIXTURES_DIR}/tree_file1.json'
+    file2_path = f'{FIXTURES_DIR}/tree_file2.json'
+    with open(f'{FIXTURES_DIR}/tree_result') as output:
+        assert generate_diff(file1_path, file2_path) == output.read()
+
+
+def test_nested_yaml_diff():
+    file1_path = f'{FIXTURES_DIR}/tree_file1.yml'
+    file2_path = f'{FIXTURES_DIR}/tree_file2.yml'
+    with open(f'{FIXTURES_DIR}/tree_result') as output:
         assert generate_diff(file1_path, file2_path) == output.read()
