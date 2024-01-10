b631cca32f8f6194703a2774733a352fb2d9f904
Egor Gerimovich <cherund@gmail.com>
2024-01-10 13:50:57 +0100

    add plain style, move styles to styles.py

--- gendiff/genereting_difference.py
+++ gendiff/genereting_difference.py
@@ -1,13 +1,6 @@
 from gendiff.parser import parse
+from gendiff.styles import plain, stylish
 import os
-
-
-def to_string(value):
-    if isinstance(value, bool):
-        return str(value).lower()
-    if value is None:
-        return 'null'
-    return value
 
 
 def get_file_data(file_path):
@@ -16,93 +9,32 @@ def get_file_data(file_path):
         return parse(file, extension)
 
 
-# def stylish(dic, indent=4):
-#     result = []
-#     for key, value in dic.items():
-#
-#         if isinstance(value, dict):
-#             value = stylish(value, indent + 4)
-#
-#         if key.startswith('+') or key.startswith('-'):
-#             result.append(f'{" " * (indent-2)}{key}: {value}')
-#         else:
-#             result.append(f'{" " * indent}{key}: {value}')
-#
-#     result_string = '\n'.join(result)
-#     return f'{{\n{result_string}\n{" " * (indent-4)}}}'
-
-
-# def comparing(file1, file2):
-#     keys = sorted(file1.keys() | file2.keys())
-#     result = {}
-#     for key in keys:
-#         if key not in file2:
-#             result[f'- {key}'] = to_string(file1[key])
-#         elif key not in file1:
-#             result[f'+ {key}'] = to_string(file2[key])
-#         elif file1[key] == file2[key]:
-#             result[f'{key}'] = to_string(file1[key])
-#         elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
-#             result[f'{key}'] = comparing(file1[key], file2[key])
-#         else:
-#             result[f'- {key}'] = to_string(file1[key])
-#             result[f'+ {key}'] = to_string(file2[key])
-#     return result
-
-
-def check_for_dict(value, indent):
-    if isinstance(value, dict):
-        return stylish(value, indent + 4)
-    return value
-
-
-def stylish(dic, indent=2):
-    result = []
-    for key, value in dic.items():
-
-
-        if value['type'] == 'deleted':
-            result.append(f'{" " * (indent)}- {key}: {value["value"]}')
-        elif value['type'] == 'added':
-            result.append(f'{" " * (indent)}+ {key}: {value["value"]}')
-        elif value['type'] == 'updated':
-            result.append(f'{" " * (indent)}- {key}: {value["value1"]}')
-            result.append(f'{" " * (indent)}+ {key}: {value["value2"]}')
-        elif value['type'] == 'unchanged':
-            result.append(f'{" " * indent}  {key}: {value["value"]}')
-        else:
-            result.append(f'{" " * indent}  {key}: {stylish(value["value"], indent+4)}')
-
-
-    result_string = '\n'.join(result)
-    return f'{{\n{result_string}\n{" " * (indent-2)}}}'
-
-
-    result_string = '\n'.join(result)
-    return f'{{\n{result_string}\n{" " * (indent-2)}}}'
-
+def formatted_dict(dic, style):
+    if style == 'plain':
+        return plain(dic)
+    return stylish(dic)
 
 def comparing(file1, file2):
     keys = sorted(file1.keys() | file2.keys())
     result = {}
     for key in keys:
         if key not in file2:
-            result[key]= {'type': 'deleted', 'value': to_string(file1[key])}
+            result[key]= {'type': 'removed', 'value': file1[key]}
         elif key not in file1:
-            result[key]= {'type': 'added', 'value': to_string(file2[key])}
+            result[key]= {'type': 'added', 'value': file2[key]}
         elif file1[key] == file2[key]:
-            result[key] = {'type': 'unchanged','value': to_string(file1[key])}
+            result[key] = {'type': 'unchanged','value': file1[key]}
         elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
             result[key] = {'type':'dictionary','value': comparing(file1[key], file2[key])}
         else:
-            result[key] = {'type': 'updated', 'value1': to_string(file1[key]),
-                           'value2': to_string(file2[key])}
+            result[key] = {'type': 'updated', 'value1': file1[key],
+                           'value2': file2[key]}
     return result
 
 
-def generate_diff(file1_path, file2_path, style=stylish):
+def generate_diff(file1_path, file2_path, style='stylish'):
     file1 = get_file_data(file1_path)
     file2 = get_file_data(file2_path)
     compared_dict = comparing(file1, file2)
-    stringed_dict = style(compared_dict)
+    stringed_dict = formatted_dict(compared_dict, style)
     return stringed_dict
---
+++ gendiff/styles.py
@@ -0,0 +1,67 @@
+def to_string(value, indent=2):
+    if isinstance(value, bool):
+        return str(value).lower()
+    if value is None:
+        return 'null'
+    if isinstance(value, dict):
+        lines = []
+        for key, val in value.items():
+            lines.append(f'{" " * indent}  {key}: {to_string(val, indent + 4)}\n')
+        return f'{{\n{"".join(lines)}{" " * (indent-2)}}}'
+    return value
+
+
+def stylish(dic, indent=2):
+    result = []
+    for key, value in dic.items():
+        if value['type'] == 'removed':
+            result.append(f'{" " * indent}- {key}: {to_string(value["value"], indent+4)}')
+        elif value['type'] == 'added':
+            result.append(f'{" " * indent}+ {key}: {to_string(value["value"], indent+4)}')
+        elif value['type'] == 'updated':
+            result.append(f'{" " * indent}- {key}: {to_string(value["value1"], indent+4)}')
+            result.append(f'{" " * indent}+ {key}: {to_string(value["value2"], indent+4)}')
+        elif value['type'] == 'unchanged':
+            result.append(f'{" " * indent}  {key}: {to_string(value["value"], indent+4)}')
+        elif value['type'] == 'dictionary':
+            result.append(f'{" " * indent}  {key}: {stylish(value["value"], indent+4)}')
+        else:
+            raise ValueError(f'Unknown type: {value["type"]}')
+
+    result_string = '\n'.join(result)
+    return f'{{\n{result_string}\n{" " * (indent-2)}}}'
+
+def checking_value(value):
+    if isinstance(value, (bool, int)):
+        return str(value).lower()
+    if value is None:
+        return 'null'
+    if isinstance(value, dict):
+        return '[complex value]'
+
+    return f"'{value}'"
+
+
+def plain(dic, path=[]):
+    result = []
+    for key, value in dic.items():
+        if 'type' in value:
+            print_path = '.'.join(path+[key])
+            if value['type'] == 'removed':
+                result.append(f"Property '{print_path}' was removed")
+            elif value['type'] == 'added':
+               result.append(f"Property '{print_path}' was added with "
+                             f"value: {checking_value(value['value'])}")
+            elif value['type'] == 'updated':
+                result.append(f"Property '{print_path}' was updated. "
+                              f"From {checking_value(value['value1'])} "
+                              f"to {checking_value(value['value2'])}")
+            elif plain(value):
+                result.append(plain(value, path+[key]))
+
+        elif isinstance(value, dict) and plain(value):
+            result.append(plain(value, path))
+
+
+    result_string = '\n'.join(result)
+    return result_string
