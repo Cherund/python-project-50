d1c2313aa95980b72ac9737b3e7cc520655b47fa
Egor Gerimovich <cherund@gmail.com>
2024-01-11 15:47:42 +0100

    add hexlet test

---
+++ tests/test_hexlet.py
@@ -0,0 +1,37 @@
+from gendiff.generating_difference import generate_diff
+from pathlib import Path
+
+
+FIXTURES_DIR = f'{Path(__file__).parent}/fixtures/hexlet'
+
+
+def test_nested_json_diff():
+    file1_path = f'{FIXTURES_DIR}/file1.json'
+    file2_path = f'{FIXTURES_DIR}/file2.json'
+
+    with open(f'{FIXTURES_DIR}/result_stylish') as output:
+        assert generate_diff(file1_path, file2_path) == output.read()
+#
+
+def test_nested_yaml_diff():
+    file1_path = f'{FIXTURES_DIR}/file1.yml'
+    file2_path = f'{FIXTURES_DIR}/file2.yml'
+
+    with open(f'{FIXTURES_DIR}/result_stylish') as output:
+        assert generate_diff(file1_path, file2_path) == output.read()
+
+
+def test_nested_json_diff_plain():
+    file1_path = f'{FIXTURES_DIR}/file1.json'
+    file2_path = f'{FIXTURES_DIR}/file2.json'
+
+    with open(f'{FIXTURES_DIR}/result_plain') as output:
+        assert generate_diff(file1_path, file2_path, 'plain') == output.read()
+
+
+def test_nested_yaml_diff_plain():
+    file1_path = f'{FIXTURES_DIR}/file1.yml'
+    file2_path = f'{FIXTURES_DIR}/file2.yml'
+
+    with open(f'{FIXTURES_DIR}/result_plain') as output:
+        assert generate_diff(file1_path, file2_path, 'plain') == output.read()
