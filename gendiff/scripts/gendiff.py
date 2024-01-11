fdef077b1758d2a112d93bf97ea7e99acd206f40
Egor Gerimovich <cherund@gmail.com>
2024-01-11 15:47:59 +0100

    add print() to script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,8 @@ from gendiff.cli import get_args
 
 def main():
     args = get_args()
-    generate_diff(args.first_file, args.second_file, args.format)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
+    print(diff)
 
 
 if __name__ == '__main__':
