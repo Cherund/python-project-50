71703b9ac7e325872d26642465d37c5916c43883
Egor Gerimovich <cherund@gmail.com>
2024-01-11 11:27:42 +0100

    add library

---
+++ gendiff/__init__.py
@@ -0,0 +1,9 @@
+from gendiff.generating_difference import generate_diff, comparing, get_file_data, formatted_dict
+
+__all__ = (
+    generate_diff,
+    comparing,
+    get_file_data,
+    formatted_dict,
+
+)
