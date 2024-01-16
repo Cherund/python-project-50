56db5db85a47f9851c4bbc250b7ca7f2d96a477f
Egor Gerimovich <cherund@gmail.com>
2024-01-16 16:01:28 +0100

    add match case

--- gendiff/styles/stylish.py
+++ gendiff/styles/stylish.py
@@ -19,25 +19,26 @@ def to_string(value, indent=2):
 def to_stylish(data, indent=2):
     result = []
     for key, value in data.items():
-        if value['type'] == 'removed':
-            result.append(f'{build_indent(indent)}- {key}: '
-                          f'{to_string(value["value"], indent + 4)}')
-        elif value['type'] == 'added':
-            result.append(f'{build_indent(indent)}+ {key}: '
-                          f'{to_string(value["value"], indent + 4)}')
-        elif value['type'] == 'updated':
-            result.append(f'{build_indent(indent)}- {key}: '
-                          f'{to_string(value["value1"], indent + 4)}')
-            result.append(f'{build_indent(indent)}+ {key}: '
-                          f'{to_string(value["value2"], indent + 4)}')
-        elif value['type'] == 'unchanged':
-            result.append(f'{build_indent(indent)}  {key}: '
-                          f'{to_string(value["value"], indent + 4)}')
-        elif value['type'] == 'dictionary':
-            result.append(f'{build_indent(indent)}  {key}: '
-                          f'{to_stylish(value["value"], indent + 4)}')
-        else:
-            raise ValueError(f'Unknown type: {value["type"]}')
+        match value['type']:
+            case 'removed':
+                result.append(f'{build_indent(indent)}- {key}: '
+                              f'{to_string(value["value"], indent + 4)}')
+            case 'added':
+                result.append(f'{build_indent(indent)}+ {key}: '
+                              f'{to_string(value["value"], indent + 4)}')
+            case 'updated':
+                result.append(f'{build_indent(indent)}- {key}: '
+                              f'{to_string(value["value1"], indent + 4)}')
+                result.append(f'{build_indent(indent)}+ {key}: '
+                              f'{to_string(value["value2"], indent + 4)}')
+            case 'unchanged':
+                result.append(f'{build_indent(indent)}  {key}: '
+                              f'{to_string(value["value"], indent + 4)}')
+            case 'dictionary':
+                result.append(f'{build_indent(indent)}  {key}: '
+                              f'{to_stylish(value["value"], indent + 4)}')
+            case _:
+                raise ValueError(f'Unknown type: {value["type"]}')
 
     result_string = '\n'.join(result)
     return f'{{\n{result_string}\n{build_indent(indent - 2)}}}'
