a860895997b066bd83109e5b9305276f24207390
Egor Gerimovich <cherund@gmail.com>
2024-01-16 15:09:27 +0100

    change for lint

--- gendiff/styles/stylish.py
+++ gendiff/styles/stylish.py
@@ -12,7 +12,7 @@ def to_string(value, indent=2):
         for key, val in value.items():
             lines.append(f'{build_indent(indent)}  '
                          f'{key}: {to_string(val, indent + 4)}\n')
-        return f'{{\n{"".join(lines)}{build_indent(indent-2)}}}'
+        return f'{{\n{"".join(lines)}{build_indent(indent - 2)}}}'
     return value
 
 
@@ -21,23 +21,23 @@ def to_stylish(data, indent=2):
     for key, value in data.items():
         if value['type'] == 'removed':
             result.append(f'{build_indent(indent)}- {key}: '
-                          f'{to_string(value["value"], indent+4)}')
+                          f'{to_string(value["value"], indent + 4)}')
         elif value['type'] == 'added':
             result.append(f'{build_indent(indent)}+ {key}: '
-                          f'{to_string(value["value"], indent+4)}')
+                          f'{to_string(value["value"], indent + 4)}')
         elif value['type'] == 'updated':
             result.append(f'{build_indent(indent)}- {key}: '
-                          f'{to_string(value["value1"], indent+4)}')
+                          f'{to_string(value["value1"], indent + 4)}')
             result.append(f'{build_indent(indent)}+ {key}: '
-                          f'{to_string(value["value2"], indent+4)}')
+                          f'{to_string(value["value2"], indent + 4)}')
         elif value['type'] == 'unchanged':
             result.append(f'{build_indent(indent)}  {key}: '
-                          f'{to_string(value["value"], indent+4)}')
+                          f'{to_string(value["value"], indent + 4)}')
         elif value['type'] == 'dictionary':
             result.append(f'{build_indent(indent)}  {key}: '
-                          f'{to_stylish(value["value"], indent+4)}')
+                          f'{to_stylish(value["value"], indent + 4)}')
         else:
             raise ValueError(f'Unknown type: {value["type"]}')
 
     result_string = '\n'.join(result)
-    return f'{{\n{result_string}\n{build_indent(indent-2)}}}'
+    return f'{{\n{result_string}\n{build_indent(indent - 2)}}}'
