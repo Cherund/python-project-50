e32a5bbafff5200c69b1010924b07b2b3aee3dad
Egor Gerimovich <cherund@gmail.com>
2024-01-16 16:01:51 +0100

    add function to tests to get file data

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,7 +2,13 @@ from gendiff.generate_difference import generate_diff
 from pathlib import Path
 import pytest
 
+
 FIXTURES_DIR = f'{Path(__file__).parent}/fixtures'
+
+
+def get_result_data(file_name):
+    with open(f'{FIXTURES_DIR}/{file_name}') as output:
+        return output.read()
 
 
 @pytest.mark.parametrize(
@@ -16,8 +22,7 @@ def test_flat_diff(file1, file2):
     file1_path = f'{FIXTURES_DIR}/{file1}'
     file2_path = f'{FIXTURES_DIR}/{file2}'
 
-    with open(f'{FIXTURES_DIR}/flat_result') as output:
-        assert generate_diff(file1_path, file2_path) == output.read()
+    assert generate_diff(file1_path, file2_path) == get_result_data('flat_result')
 
     with open(f'{FIXTURES_DIR}/flat_plain_result') as output:
         assert generate_diff(file1_path, file2_path, 'plain') == output.read()
