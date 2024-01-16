5f7ef219093512cf58f0faf5abde4d405425261c
Egor Gerimovich <cherund@gmail.com>
2024-01-16 15:09:56 +0100

    change plain_style.py

--- gendiff/styles/plain_style.py
+++ gendiff/styles/plain_style.py
@@ -9,33 +9,46 @@ def checking_value(value):
     return f"'{value}'"
 
 
-def to_plain(data, path=''):
-    result = []
-    for key, value in data.items():
-        if 'type' in value:
+def to_plain(data):
+    def _iter_plain(data, path=''):
+        result = []
+        for key, value in data.items():
             print_path = f"{path}.{key}" if path else f'{key}'
-            if value['type'] == 'removed':
-                result.append(f"Property '{print_path}' was removed")
-            elif value['type'] == 'added':
-                result.append(f"Property '{print_path}' was added with "
-                              f"value: {checking_value(value['value'])}")
-            elif value['type'] == 'updated':
-                result.append(f"Property '{print_path}' was updated. "
-                              f"From {checking_value(value['value1'])} "
-                              f"to {checking_value(value['value2'])}")
-            else:
-                if key == 'value':
-                    appending_value = to_plain(value, path)
-                else:
-                    appending_value = to_plain(value, print_path)
+            match value['type']:
+                case 'removed':
+                    result.append(f"Property '{print_path}' was removed")
+                case 'added':
+                    result.append(f"Property '{print_path}' was added with "
+                                  f"value: {checking_value(value['value'])}")
+                case 'updated':
+                    result.append(f"Property '{print_path}' was updated. "
+                                  f"From {checking_value(value['value1'])} "
+                                  f"to {checking_value(value['value2'])}")
+                case 'dictionary':
+                    result.extend(_iter_plain(value['value'], print_path))
+                case 'unchanged':
+                    continue
+                case _:
+                    raise ValueError(f'Unknown type: {value["type"]}')
 
-                if appending_value:
-                    result.append(appending_value)
+            #
+            # if value['type'] == 'removed':
+            #     result.append(f"Property '{print_path}' was removed")
+            # elif value['type'] == 'added':
+            #     result.append(f"Property '{print_path}' was added with "
+            #                   f"value: {checking_value(value['value'])}")
+            # elif value['type'] == 'updated':
+            #     result.append(f"Property '{print_path}' was updated. "
+            #                   f"From {checking_value(value['value1'])} "
+            #                   f"to {checking_value(value['value2'])}")
+            # elif value['type'] == 'dictionary':
+            #     result.extend(_iter_plain(value['value'], print_path))
+            # elif value['type'] == 'unchanged':
+            #     continue
+            # else:
+            #     raise ValueError(f'Unknown type: {value["type"]}')
 
-        elif isinstance(value, dict):
-            appending_value = to_plain(value, path)
-            if appending_value:
-                result.append(to_plain(value, path))
+        return result
 
-    result_string = '\n'.join(result)
+    result_string = '\n'.join(_iter_plain(data))
     return result_string
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,13 +1,20 @@
 from gendiff.generate_difference import generate_diff
 from pathlib import Path
-
+import pytest
 
 FIXTURES_DIR = f'{Path(__file__).parent}/fixtures'
 
 
-def test_flat_json_diff():
-    file1_path = f'{FIXTURES_DIR}/file1.json'
-    file2_path = f'{FIXTURES_DIR}/file2.json'
+@pytest.mark.parametrize(
+    ('file1', 'file2'),
+    [
+        ('file1.json', 'file2.json'),
+        ('file1.yml', 'file2.yml'),
+    ]
+)
+def test_flat_diff(file1, file2):
+    file1_path = f'{FIXTURES_DIR}/{file1}'
+    file2_path = f'{FIXTURES_DIR}/{file2}'
 
     with open(f'{FIXTURES_DIR}/flat_result') as output:
         assert generate_diff(file1_path, file2_path) == output.read()
@@ -19,23 +26,16 @@ def test_flat_json_diff():
         assert generate_diff(file1_path, file2_path, 'json') == output.read()
 
 
-def test_flat_yml_diff():
-    file1_path = f'{FIXTURES_DIR}/file1.yml'
-    file2_path = f'{FIXTURES_DIR}/file2.yml'
-
-    with open(f'{FIXTURES_DIR}/flat_result') as output:
-        assert generate_diff(file1_path, file2_path) == output.read()
-
-    with open(f'{FIXTURES_DIR}/flat_plain_result') as output:
-        assert generate_diff(file1_path, file2_path, 'plain') == output.read()
-
-    with open(f'{FIXTURES_DIR}/flat_json_result') as output:
-        assert generate_diff(file1_path, file2_path, 'json') == output.read()
-
-
-def test_nested_json_diff():
-    file1_path = f'{FIXTURES_DIR}/nested_file1.json'
-    file2_path = f'{FIXTURES_DIR}/nested_file2.json'
+@pytest.mark.parametrize(
+    ('file1', 'file2'),
+    [
+        ('nested_file1.json', 'nested_file2.json'),
+        ('nested_file1.yml', 'nested_file2.yml'),
+    ]
+)
+def test_nested_json_diff(file1, file2):
+    file1_path = f'{FIXTURES_DIR}/{file1}'
+    file2_path = f'{FIXTURES_DIR}/{file2}'
 
     with open(f'{FIXTURES_DIR}/nested_result') as output:
         assert generate_diff(file1_path, file2_path) == output.read()
@@ -45,27 +45,3 @@ def test_nested_json_diff():
 
     with open(f'{FIXTURES_DIR}/nested_json_result') as output:
         assert generate_diff(file1_path, file2_path, 'json') == output.read()
-
-
-def test_nested_yaml_diff():
-    file1_path = f'{FIXTURES_DIR}/nested_file1.yml'
-    file2_path = f'{FIXTURES_DIR}/nested_file2.yml'
-
-    with open(f'{FIXTURES_DIR}/nested_result') as output:
-        assert generate_diff(file1_path, file2_path) == output.read()
-
-    with open(f'{FIXTURES_DIR}/nested_plain_result') as output:
-        assert generate_diff(file1_path, file2_path, 'plain') == output.read()
-
-    with open(f'{FIXTURES_DIR}/nested_json_result') as output:
-        assert generate_diff(file1_path, file2_path, 'json') == output.read()
-
-
-# def test_nest():
-#     file1_path = f'{FIXTURES_DIR}/nested_file1.json'
-#     file2_path = f'{FIXTURES_DIR}/nested_file2.json'
-#
-#     print(generate_diff(file1_path, file2_path))
-#
-#
-# test_nest()
